2b8dab34135f3daed1ad3d96be00c313f82c7247
HAMIDI massinissa <hamidi@lucien.informatique.univ-paris-diderot.fr>
2017-01-16 14:04:13 +0100

    starting to deal with unification algorithm

--- semantics.ml
+++ semantics.ml
@@ -10,36 +10,32 @@ type program =
    | TyBase of tybase
    | TyProduct of ty * ty
    | TyArrow of ty * ty
-                       
+
  and tybase = Int | Bool
-                      
+
  and variable = string
 
  and typed_variable = VarTyped of variable * ty
-                              
+
  and constant = string
 
 module StringSet = Set.Make(String)
-    
+
 let rec free_variables prog =
   match prog with
   | Var s ->
   (* StringSet.add s StringSet.empty *)
      StringSet.singleton s
-                   
   | Const _ ->
      StringSet.empty
-       
   | Pair (p1, p2) | App (p1, p2) ->
-     let l1 = free_variables p1 
+     let l1 = free_variables p1
      and l2 = free_variables p2 in
      StringSet.union l1 l2
-                     
   | Lambda (tv, p) ->
      let l = free_variables p in
      let VarTyped (s, ty) = tv in
      StringSet.remove s l
-                      
   | Let (tv, p1, p2) ->
      let l1 = free_variables p1
      and l2 = free_variables p2 in
@@ -74,7 +70,7 @@ let variable_clashes p subst =
   let free_vars = List.fold_left StringSet.union StringSet.empty subst_freed
   and bound_vars = bound_variables p in
   StringSet.inter free_vars bound_vars
-  
+
 let rec apply_substitution prog subst =
   match prog with
   | Var s as v ->
@@ -113,18 +109,17 @@ let rec apply_substitution prog subst =
   | Let (tv, p1, p2) ->
      (** ... also here *)
      let p1_subst = apply_substitution p1 subst in
-     let VarTyped (s, _) = tv in
-     let subst' = (s, p1_subst)::subst in
+     (*let VarTyped (s, _) = tv in
+      let subst' = (s, p1_subst)::subst in *)
      let clashes = variable_clashes p2 (List.(snd (split subst))) in
      let p2_subst =
        if (StringSet.is_empty clashes)
-       then apply_substitution p2 subst'
+       then apply_substitution p2 subst
        else (** alpha_conversion *)
          let p2_alpha_converted = alpha_conversion p2 in
-         apply_substitution p2_alpha_converted subst'
+         apply_substitution p2_alpha_converted subst
      in
-     Let (tv, p1_subst, p2_subst) 
-     
+     Let (tv, p1_subst, p2_subst)
 
 let rec bigstep_eval prog =
   match prog with
@@ -153,7 +148,6 @@ let rec curry_monomorph env term =
        with
          Not_found -> failwith "Unbound variable"
      end
-       
   | Const s ->
      begin
        try
@@ -161,12 +155,10 @@ let rec curry_monomorph env term =
        with
          Not_found -> failwith "Unknown constant"
      end
-     
   | Pair (p1, p2) ->
      let ty_p1 = curry_monomorph env p1 in
      let ty_p2 = curry_monomorph env p2 in
      TyProduct(ty_p1, ty_p2)
-              
   | App (p1, p2) ->
      let ty_p1 = curry_monomorph env p1 in
      begin match ty_p1 with
@@ -177,12 +169,10 @@ let rec curry_monomorph env term =
         else failwith "Argument type mismatch in application"
      | _ -> failwith "Not an application"
      end
-                        
   | Lambda (tv, p) ->
      let VarTyped (s, ty_v) = tv in
      let tyB = curry_monomorph ((s, ty_v)::env) p in
      TyArrow(ty_v, tyB)
-        
   | Let (v, p1, p2) ->
      let VarTyped (s, tyA) = v in
      let tyA_expected = curry_monomorph env p1 in
