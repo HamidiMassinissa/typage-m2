53be86ef2ffd2376b11f85a8ff4dbfa74aef25d7
Massinissa Hamidi <hamidi@informatique.univ-paris-diderot.fr>
2017-02-01 19:36:22 +0100

    almost done with unification algorithm

--- unification.ml
+++ unification.ml
@@ -27,7 +27,69 @@ let variables_of_term (t:term) : variable list =
     | Var v -> v::acc
     | Fun (f, ts) -> List.fold_left aux acc ts
   in
-  aux [] t
+  aux [] t;;
+
+let replace_and_check (eqs: equational_system) (var: variable) (term: term) =
+  let r = ref false in
+  let rec aux t =
+    match t with
+    | Cons _ -> t
+    | Var x when x = var ->
+       r := true; term
+    | Var x -> t
+    | Fun (f,ts) ->
+       let ts' =
+	 List.map (fun t -> aux t) ts
+       in
+       Fun (f,ts')
+  in
+  !r,
+  List.map
+    (fun (Equality (s,t)) ->
+     let s' = aux s in
+     let t' = aux t in
+     Equality (s', t')) eqs
+
+exception Found_variable
+
+let f term var =
+  let rec aux =
+    function
+    | Var x when x=var -> true
+    | Fun (_,ts) -> List.for_all aux ts
+    | Var _ | Cons _ -> false
+  in
+  aux term
+
+let belongs_to_var (eqs:equational_system) var =
+  List.for_all
+    (fun
+	(Equality (s,t)) ->
+	  let s' = f s var in
+	  let t' = f t var in
+	  s'&&t'
+	) eqs
+
+let rec occur_check term var =
+  f term var
+
+let replace eqs var term =
+  let rec aux =
+    function
+    | Cons _ as t-> t
+    | Var x when x=var -> term
+    | Var x as t -> t
+    | Fun (f,ts) ->
+       let ts' = List.map (fun t -> aux t) ts in
+       Fun (f,ts')
+  in
+  List.map
+    (fun
+	(Equality (s,t)) ->
+	 let s' = aux s in
+	 let t' = aux t in
+	 Equality (s',t')
+	) eqs
 
 (** The Unification Algorithm [unify]
 
@@ -41,18 +103,18 @@ let variables_of_term (t:term) : variable list =
         - else fail
 
  *)
-let rec unify (e:equational_system) : substitution =
+let rec unify (e:equational_system) : equational_system =
   match e with
     [] -> []
-  | Equality (t1, t2)::t ->
+  | (Equality (t1, t2) as eq)::eqs ->
      match t1, t2 with
      | t1, t2 when t1 = t2 -> (* erase *)
         (* we check structural equality with '=' *)
-        unify t
+        unify eqs
 
      | t1, Var x -> (* orient *)
         (* TODO find a way to ensure that t1 is not a variable *)
-        unify (Equality (Var x, t1)::t)
+        unify (Equality (Var x, t1)::eqs)
 
      | Fun (f1,l1), Fun (f2,l2) -> (* decompose *)
         let len_1 = List.length l1
@@ -61,18 +123,40 @@ let rec unify (e:equational_system) : substitution =
         then failwith "decompose rule: Terms have different arities"
         else
           let enriched_e =
-            List.fold_left2 (fun acc e1 e2 -> Equality (e1, e2)::acc) t l1 l2
+            List.fold_left2 (fun acc e1 e2 -> Equality (e1, e2)::acc) eqs l1 l2
           in
           unify enriched_e
 
-     | Var x as t1, t2 -> (* replace *)
-        (* check whether x belongs to Var(E) -Done by pattern matching ... *)
-        (* ... and x doesn't belong to Var(t2) *)
-        let vars_of_t2 = variables_of_term t2 in
-	[]
+     | Var x, s when belongs_to_var eqs x -> (* replace: EU{x=s} *)
+	let occur = occur_check s x in
+	if (occur)
+	then
+	  failwith (Printf.sprintf
+	       "replace rule: occur check -
+		variable %s appears in the right term" x)
+	else
+	  let eqs' = replace eqs x s in
+	  unify eqs'@[eq]
 
      | _, _ -> (* No rule is applicable *)
-        unify t
+        unify eqs
+
+(* [f(x, h(b), c) = f(g(y), y, c); x = c] *)
+let eq0 = [ Equality(
+                Fun ("f",[Var "x"; Fun("h", [Cons "b"]); Cons "c"]),
+                Fun ("f",[Fun("g",[Var "y"]); Var "y"; Cons "c"]));
+            Equality(Var "x", Cons "c")]
+let eq0' = unify eq0
+
+(* [x=a; y=b] *)
+let eq1 = [ Equality(Var "x", Cons "a"); Equality(Var "y", Cons "b") ]
+let eq1' = unify eq1
+
+(* [x=g(y); y=b] *)
+let eq2 = [ Equality(Var "x", Fun("g", [Var "y"]));
+	    Equality(Var "y", Cons "b") ]
+let eq2' = unify eq2
+
 
 (** The equational system E={s1=t1, ... , sn=tn} is in solved form iff
   (i)  All the si are distinct variables
@@ -81,26 +165,17 @@ let rec unify (e:equational_system) : substitution =
  *)
 let solved_form equational_system = true
 
-let solve e =
-  let s = unify e in
-  if (solved_form s) then s
-  else failwith "The equational system does not have a solution"
-
-
-let fake_eq_sys = [ Equality(
-                        Fun ("f",[Var "x"; Fun("h", [Cons "b"]); Cons "c"]),
-                        Fun ("f",[Fun("g",[Var "y"]); Var "y"; Cons "c"]));
-                    Equality(Var "x", Cons "c")]
-
-let _ = solve fake_eq_sys
-
-
-
-(** deprecated [to_substitution] *)
 let to_substitution equational_system : substitution =
   List.map
     (fun (Equality(t1, t2)) ->
       match t1 with
       | Var s -> (s, t2)
-      | _ -> assert false)
+      | _ -> failwith "The given equational system is not in resolved form!")
     equational_system
+
+let solve e =
+  let s = unify e in
+  if (solved_form s) then s
+  else failwith "The equational system does not have a solution"
+
+let _ = solve eq0
