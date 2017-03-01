eac0cbeffa655946775bfafd45d7ccfed8dd6881
HAMIDI massinissa <hamidi@informatique.univ-paris-diderot.fr>
2017-03-01 13:40:34 +0100

    Added missing prettyPrinter in the last commit

---
+++ src/prettyPrinter.ml
@@ -0,0 +1,35 @@
+open Ast
+open Unification
+
+let rec typescheme_to_string t =
+  match t with
+  | TypeVar tv -> tv
+  | TypeBase tid -> tid
+  | TypeArrow (tscha,tschb) ->
+     typescheme_to_string tscha ^ " → " ^
+       typescheme_to_string tschb
+  | TypeProduct (tscha,tschb) ->
+     typescheme_to_string tscha ^ " × " ^
+       typescheme_to_string tschb
+
+let tconstraint_to_string t =
+  let aconstraint = function
+    | TyEq (a, b) ->
+      Printf.sprintf "%s =?= %s"
+        (typescheme_to_string a)
+        (typescheme_to_string b)
+  in
+  "[\n" ^ String.concat "\n" (List.map aconstraint t) ^ "\n]"
+
+let rec term_to_string t =
+  match t with
+  | Const c -> c
+  | Var v -> v
+  | Pair (n,l) ->
+     "<" ^ term_to_string n ^ "," ^ term_to_string l ^ ">"
+  | Lambda (x,n) ->
+     "λ" ^ x ^ "." ^ term_to_string n
+  | App (n,l) ->
+     term_to_string n ^ " " ^ term_to_string l
+  | Let (x,n,l) ->
+     "let " ^ x ^ "=" ^ term_to_string n ^ " in " ^ term_to_string l
